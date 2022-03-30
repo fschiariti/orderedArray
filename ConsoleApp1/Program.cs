27b13f9607fd8dd12a5113e31213260a8f67d7a1
Fernando <fernando@whitecloudwt.com>
2022-03-30 13:10:01 -0300

    Order an array challenge without using Array.Sort

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int tmp = 0;
+            int[] arreglo = { 99, 98, 1, 3, 2, 12, 6 };
+
+            for (int i = 0; i < arreglo.Count(); i++)
+            {
+
+                for (int j = 0; j < arreglo.Count()-1; j++)
+                {
+                    if ( arreglo[j] > arreglo[j + 1])
+                    {
+                        tmp = arreglo[j];
+                        arreglo[j] = arreglo[j + 1];
+                        arreglo[j + 1] = tmp;
+
+                    }
+
+                }
+
+            }
+
+
+            for (int i = 0; i < arreglo.Length ; i++)
+            {
+                Console.WriteLine(arreglo[i]);
+            }
+
+        }
+    }
+}
